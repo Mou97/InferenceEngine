a626207289ea6ac48d8e5aff17920e9c1ba43242
Mou97 <m.taguiouine@esi-sba.dz>
2019-12-25 23:22:19 +0100

    read inputs from a file

--- src/app/App.java
+++ src/app/App.java
@@ -1,34 +1,29 @@
 package app;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import app.Br;
 import app.ChainageAvant;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        String f = "H";
 
+    public static void main(String[] args) throws Exception {
+        // fait
+        String f = new String();
+        ArrayList<String> fVal = new ArrayList<String>();
+       
         // base de faits
         ArrayList<String> bf = new ArrayList<String>();
-        bf.add("C");
-        bf.add("B");
-
         // base de regles
-        Regle r1 = new Regle("B,D,E", "F");
-        Regle r2 = new Regle("G,D", "A");
-        Regle r3 = new Regle("C,F", "A");
-        Regle r4 = new Regle("B", "X");
-        Regle r5 = new Regle("D", "E");
-        Regle r6 = new Regle("X,A", "H");
-        Regle r7 = new Regle("C", "D");
-        Regle r8 = new Regle("X,C", "A");
-        Regle r9 = new Regle("X,B", "D");
-
-
-
         Br bRegle = new Br();
-        bRegle.addRegle(r1).addRegle(r2).addRegle(r3).addRegle(r4).addRegle(r5).addRegle(r6).addRegle(r7).addRegle(r8).addRegle(r9);
-       
+
+        // read date from file
+        readFromFile( fVal,  bf,  bRegle );
+        // workaround for global string variable problem
+        f+=fVal.get(0);
 
         // Print base de faits;
         System.out.println(bRegle.toString());
@@ -41,5 +36,38 @@ public class App {
         // System.out.println(bf.toString());
         // System.out.println(br.toString());
         // System.out.println(bRegle);
+    }
+
+    public static void readFromFile(ArrayList<String> f, ArrayList<String> bf, Br bRegle){
+        BufferedReader reader;
+        
+        try {
+            reader = new BufferedReader(new FileReader("data/in.txt"));
+            // read first line 
+            String line = reader.readLine();
+            f.add(line.strip());
+            
+            // read second line
+            line = reader.readLine();
+            bf.addAll(Arrays.asList(line.split(",")));
+            System.out.println(bf.toString());
+
+            line= reader.readLine();
+            
+            Regle r ;
+            String [] temp ;
+            while(line !=null){
+                temp = line.split(" ");
+                r = new Regle(temp[0].strip(), temp[1].strip());
+                bRegle.addRegle(r);
+                line= reader.readLine();  
+
+            }
+            reader.close();
+            
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
     }
 }
