b20a2bdeb8af72101c0ebf692af4ae2a6fa58b58
Mou97 <m.taguiouine@esi-sba.dz>
2019-12-26 14:20:48 +0100

    print the rule's tree in the order used for demonstration process

--- src/app/ChainageAvant.java
+++ src/app/ChainageAvant.java
@@ -4,18 +4,28 @@ import java.util.ArrayList;
 
 public class ChainageAvant{
     public static void solve(ArrayList<String> bf,Br bRegle,String f){
+        ArrayList<Regle> treeRegle = new ArrayList<Regle>();
         try {
             while (!bf.contains(f) && bRegle.countRegleApplicable(bf)>0) {
+                // get applicable rule 
                 Regle regAppl =  bRegle.getRegleApplicable(bf);
+                // deactivate the rule
                 regAppl.deactivate();
-                // TODO: add regApp to a stack
                 System.out.println("la regle applicable est : \n"+regAppl.toString()+"\n");
+                // add regApp to a stack
+                treeRegle.add(regAppl);
+                // add the actions to BF
                 bf.addAll(regAppl.action);
                 System.out.println("BF pour cet iteration:  "+ bf.toString() + "\n");
 
             }
             if(bf.contains(f)){
                 System.out.println( "La regle: " + f + "  est etabli");
+                System.out.println("Les regles utilisees pour demontrer {" + f +"} :");
+                for (Regle r : treeRegle) {
+                    System.out.println(r);
+                }
+                
             }else{
                 System.out.println( "La regle: " + f + "  n'est pas etabli");
 
@@ -23,11 +33,10 @@ public class ChainageAvant{
         } catch (Exception e) {
                 System.out.println(e);
         }
-        // System.out.println(!bf.contains(f));
+        
 
        
     }
 }
 
 
-// System.out.println();
